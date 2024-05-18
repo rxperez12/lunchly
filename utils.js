b3c4aadf99d5373695a9e08f6dbf8340c0de2cd1
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-05-17 21:15:18 -0400

    Fixed doc string.

--- utils.js
+++ utils.js
@@ -1,14 +1,12 @@
 
-/**
- * Take in string
- * Returns array with string split by spaces, capitalizes first letter,
- * and make the rest lower case
+/** Take in string
+ *  Returns array with string split by spaces, capitalizes first letter,
+ *  and make the rest lower case
  *
- * Input:
- * 'aubrey sherman'
- * Output:
- * ['Aubrey', 'Sherman']
- * FIXME: change function name
+ *  Input:
+ *  'aubrey sherman'
+ *  Output:
+ *  ['Aubrey', 'Sherman']
  */
 function formatName(name) {
   return name.trim().split(' ').map(name => {
